57d088c1f103c3b792034a150ae50cff74a2dfd8
tetiana_baranova <syniovych.tanya@gmail.com>
2026-05-07 14:37:20 +0300

    update app

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from pytesseract import image_to_string
 # якщо PATH не налаштований, розкоментуй:
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-text = image_to_string(Image.open("text.png"))
+text = image_to_string(Image.open("ukr.png"), lang="ukr+eng")
 print(text)
 
